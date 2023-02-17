14a80c3d78335777ba0be993f2fdcc200929b548
ali <alialijani1377@gmail.com>
2023-02-17 22:49:05 +0330

    AddDbContext

---
+++ PhoneNumber.DataLayer/Context/PhoneNumberDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumber.DataLayer.Context
+{
+	public class PhoneNumberDbContext : DbContext
+	{
+		#region ctor
+		//public PhoneNumberDbContext()
+		//{
+
+		//}
+		public PhoneNumberDbContext(DbContextOptions<PhoneNumberDbContext> options) : base(options)
+		{
+
+		}
+		#endregion
+
+		#region DbSet
+		public DbSet<PhoneNumber.Domain.Entities.PhoneNumber> phoneNumbers { get; set; }
+		#endregion
+
+		//protected override void OnConfiguring(DbContextOptionsBuilder options)
+		//{
+		//	if (!options.IsConfigured)
+		//	{
+		//		options.UseSqlServer("Data Source = .; Initial Catalog = PhoneNumberDb; Integrated Security = true; MultipleActiveResultSets=true; TrustServerCertificate=True; Trusted_Connection=True;");
+		//	}
+		//}
+
+		//protected override void OnModelCreating(ModelBuilder modelBuilder)
+		//{
+		//	base.OnModelCreating(modelBuilder);
+		//}
+	}
+}
---
+++ PhoneNumber.DataLayer/Migrations/20230217191755_CreateDataBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneNumber.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "phoneNumbers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Family = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_phoneNumbers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "phoneNumbers");
+        }
+    }
+}
